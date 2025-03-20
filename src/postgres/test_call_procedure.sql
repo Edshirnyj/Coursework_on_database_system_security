aef2a818104c7e35fe740fc53ded77948164b20c
Rodion P. <49054722+Edshirnyj@users.noreply.github.com>
2025-03-20 16:15:57 +0600

    Cleaning project

--- src/postgres/test_call_procedure.sql
+++
@@ -1,6 +0,0 @@
-CALL AddNewClient('Иван', 'Иванович', 'Иванов',
-    pgp_sym_encrypt('1234567890', 'test-key'),
-    pgp_sym_encrypt('ivan2329@example.test', 'test-key'),
-    pgp_sym_encrypt('3892', 'test-key'),
-    pgp_sym_encrypt('480923', 'test-key')
-);
